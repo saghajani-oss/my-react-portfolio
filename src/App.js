46f86a792d6bfeb1c0f4996dbfc2568ab088dfc8
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2023-05-03 09:33:08 +0430

    First react setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+    <h3>APP</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/experience/Experience.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./experience.css"
+
+export default function experience() {
+    return(
+        <div className="experience"></div>
+    )
+}
